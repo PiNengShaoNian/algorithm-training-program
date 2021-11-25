811259b89331161a35c9c5f2fbd140a74fc63420
瞿贵友 <quguiyou1076@qq.com>
2021-11-25 14:55:16 +0800

    length-of-longest-fibonacci-subsequence动态规划解法

---
+++ src/dynamic-programming/__test__/length-of-longest-fibonacci-subsequence.test.ts
@@ -0,0 +1,5 @@
+import { lenLongestFibSubseq1 } from '../length-of-longest-fibonacci-subsequence'
+
+test('lenLongestFibSubseq1', () => {
+  expect(lenLongestFibSubseq1([1, 2, 3, 4, 5, 6, 7, 8])).toBe(5)
+})
--- src/dynamic-programming/length-of-longest-fibonacci-subsequence.ts
+++ src/dynamic-programming/length-of-longest-fibonacci-subsequence.ts
@@ -19,3 +19,29 @@ export function lenLongestFibSubseq(arr: number[]): number {
 
   return ans >= 3 ? ans : 0
 }
+
+//动态规划解法时间复杂度O(N^2)
+export function lenLongestFibSubseq1(arr: number[]): number {
+  const numToIndex = new Map<number, number>()
+  const n = arr.length
+  for (let i = 0; i < n; ++i) {
+    numToIndex.set(arr[i], i)
+  }
+
+  // dp.get(i * n + j)其中i < j表示以arr[i] + arr[j]为尾项的斐波那契序列长度为dp.get(i * n + j) - 2
+  const dp = new Map<number, number>()
+
+  let ans = 0
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < i; ++j) {
+      if (arr[i] - arr[j] < arr[j] && numToIndex.has(arr[i] - arr[j])) {
+        const first = numToIndex.get(arr[i] - arr[j])!
+
+        dp.set(j * n + i, (dp.get(first * n + j) ?? 0) + 1)
+        ans = Math.max(ans, dp.get(j * n + i)! + 2)
+      }
+    }
+  }
+
+  return ans >= 3 ? ans : 0
+}
