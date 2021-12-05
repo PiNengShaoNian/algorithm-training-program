5d1141ce9fd3e57445f82aabc069489d2e43c8ea
瞿贵友 <quguiyou1076@qq.com>
2021-12-05 15:12:20 +0800

    longest-palindromic-subsequence

--- src/dynamic-programming/longest-palindromic-subsequence.ts
+++ src/dynamic-programming/longest-palindromic-subsequence.ts
@@ -24,3 +24,26 @@ export function longestPalindromeSubseq(s: string): number {
 
   return dp[n][n]
 }
+
+export function longestPalindromeSubseq1(s: string): number {
+  //dp[i][j]代表s[i,j]所代表的字串中包含的最长回文子序列长度为dp[i][j]
+  const dp: number[][] = []
+  const n = s.length
+
+  for (let i = 0; i < n; ++i) {
+    dp[i] = Array.from<number>({ length: n }).fill(0)
+  }
+
+  for (let i = n - 1; i >= 0; --i) {
+    dp[i][i] = 1
+    for (let j = i + 1; j < n; ++j) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
+      }
+    }
+  }
+
+  return dp[0][n - 1]
+}
