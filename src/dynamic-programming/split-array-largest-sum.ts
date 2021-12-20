9d736a687dcdba1db83bcc7bd806765a3e187a18
瞿贵友 <quguiyou1076@qq.com>
2021-12-20 16:06:09 +0800

    split-array-largest-sum二分贪心解法

--- src/dynamic-programming/split-array-largest-sum.ts
+++ src/dynamic-programming/split-array-largest-sum.ts
@@ -1,3 +1,4 @@
+//动态规划解法时间复杂度O(N ^ 2 * m)
 export function splitArray(nums: number[], m: number): number {
   const n = nums.length
   //dp[i][j]表示将前i个元素分为j组后这些子数组各自和的最大值能达到的最小值
@@ -29,4 +30,41 @@ export function splitArray(nums: number[], m: number): number {
   }
 
   return dp[n][m]
+}
+
+//动态规划解法O(N * log(10 ^ 9))
+export function splitArray1(nums: number[], m: number): number {
+  let right = 10 ** 9
+  let left = 0
+
+  while (left <= right) {
+    //枚举最大的子数组和为mid
+    const mid = (right + left) >> 1
+
+    let count = 1
+    let sum = 0
+    //算出以mid为子数组和能将数组分割为几个子数组
+    for (let i = 0; i < nums.length; ++i) {
+      //如果nums[i]大于mid说明子数组和太小无法分割
+      if (nums[i] > mid) {
+        count = Infinity
+        break
+      }
+      if (sum + nums[i] > mid) {
+        ++count
+        sum = nums[i]
+      } else {
+        sum += nums[i]
+      }
+    }
+
+    //该mid能分割出答案,尝试更小的mid
+    if (count <= m) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return left
 }
