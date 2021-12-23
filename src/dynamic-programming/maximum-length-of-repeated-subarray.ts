72a70f217b3e3b754990656dea6608c859ba9795
瞿贵友 <quguiyou1076@qq.com>
2021-12-23 11:07:40 +0800

    maximum-length-of-repeated-subarray动态规划解法

--- src/dynamic-programming/maximum-length-of-repeated-subarray.ts
+++ src/dynamic-programming/maximum-length-of-repeated-subarray.ts
@@ -1,4 +1,4 @@
-//自己做题O(n * m)
+//自己做题,复杂度O(n ^ 3)
 export function findLength(nums1: number[], nums2: number[]): number {
   let ans = 0
   const n = nums1.length
@@ -18,6 +18,31 @@ export function findLength(nums1: number[], nums2: number[]): number {
   for (let i = 0; i < n; ++i) {
     for (let j = 0; j < m; ++j) {
       ans = Math.max(getCommonArrayLength(i, j), ans)
+    }
+  }
+
+  return ans
+}
+
+//动态规划解法，时间复杂度O(n * m)
+export function findLength1(nums1: number[], nums2: number[]): number {
+  const n = nums1.length
+  const m = nums2.length
+  //dp[i][j]表示在nums1中以nums1[i]元素为开头的子数组
+  //和nums2中以nums2[j]为开头的子数组最大的公共前缀长度
+  //A: [1,2,3,2,1]
+  //B: [3,2,1,4,7]
+  //就比如dp[3][1]就为2,因为数组[2,1]和数组[2,1,4,7]最大的公共前缀长度为2
+  
+  const dp: number[][] = Array.from({ length: n + 1 }, () =>
+    Array.from<number>({ length: m + 1 }).fill(0)
+  )
+
+  let ans = 0
+  for (let i = n - 1; i >= 0; --i) {
+    for (let j = m - 1; j >= 0; --j) {
+      dp[i][j] = nums1[i] === nums2[j] ? dp[i + 1][j + 1] + 1 : 0
+      ans = Math.max(dp[i][j], ans)
     }
   }
 
