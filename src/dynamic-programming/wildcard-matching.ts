6231070dad2e3b42bfb4d8f72ad828baa4c07986
瞿贵友 <quguiyou1076@qq.com>
2021-12-24 15:33:42 +0800

    wildcard-matching贪心解法

--- src/dynamic-programming/wildcard-matching.ts
+++ src/dynamic-programming/wildcard-matching.ts
@@ -47,3 +47,91 @@ function isMatch(s: string, p: string): boolean {
 
   return !!dp[n][m]
 }
+
+//贪心解法
+export function isMatch1(s: string, p: string): boolean {
+  //由于连续的星号的作用等价于一个星号
+  //所以把模式串看成这样的一个串 `*u1*u2*u3*...un*`
+  //问题就变成在s中去找是否存在顺序的u1,u2,u3,...,un这样的字串
+
+  const charMatch = (a: string, b: string): boolean => {
+    return a === b || b === '?'
+  }
+
+  const allStars = (l: number, r: number): boolean => {
+    for (let i = l; i < r; ++i) {
+      if (p[i] !== '*') return false
+    }
+
+    return true
+  }
+
+  //当前s遍历到的位置
+  let sIndex = 0
+  //当前p遍历到的位置
+  let pIndex = 0
+  let sLen = s.length
+  let pLen = p.length
+
+  //特殊处理末尾不是星号的情况，一直裁剪末尾字符直到遇到一个星号
+  while (pLen && sLen && p[pLen - 1] !== '*') {
+    if (charMatch(s[sLen - 1], p[pLen - 1])) {
+      --pLen
+      --sLen
+    } else {
+      return false
+    }
+  }
+
+  if (!pLen) {
+    return !sLen
+  }
+
+  //字串ui在s中的起点
+  let sRecord = -1
+  //字串ui在p中的起点
+  let pRecord = -1
+
+  while (sIndex < sLen && pIndex < pLen) {
+    if (p[pIndex] === '*') {
+      //遇到了一个星号，记录新的u_i起点
+      ++pIndex
+      pRecord = pIndex
+      sRecord = sIndex
+    } else if (charMatch(s[sIndex], p[pIndex])) {
+      ++sIndex
+      ++pIndex
+    } else if (sRecord !== -1 && sRecord + 1 < sLen) {
+      //该字符不能匹配，那就让前面的星号吃掉他,u_i字串的范围从原来的[sRecord, x]
+      //变成[sRecord + 1, x],注意x是不确定的只有遇到了下一个星号才直到
+      sRecord += 1
+      sIndex = sRecord
+      //让pIndex复位重新开始尝试
+      pIndex = pRecord
+    } else {
+      //这里处理了模式串开头不为星号的情况
+      //s = abc
+      //p = def
+      //sIndex = 0,pIndex = 0
+      //因为charMatch(s[0],p[0])返回false,并且sReocrd = -1
+      //这里会直接返回false
+      //当然如果尝试过了所有位置也找不到字串u_i也会进入到这里
+      return false
+    }
+  }
+
+  //考虑如下情况
+  // s = abc
+  // p = ab*d*
+  //这种情况下前面的前缀ab会被首先匹配掉
+  // s = c
+  // p = *d*
+  //遇到了星号，pIndex + 1,sIndex不变
+  // s = c
+  // p = d*
+  // 然后c和d不匹配，选择用d前面的星号把c吃了
+  // s = ""
+  // p = d*
+  // sIndex到达末尾退出循环，只有p中剩余的全部是星号才算匹配上了
+  return allStars(pIndex, pLen)
+}
