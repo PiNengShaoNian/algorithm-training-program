91057b4cb8144ab3025148689a35e05b9ac36e37
瞿贵友 <quguiyou1076@qq.com>
2022-01-05 14:16:25 +0800

    2-keys-keyboard优化

--- src/dynamic-programming/2-keys-keyboard.ts
+++ src/dynamic-programming/2-keys-keyboard.ts
@@ -1,3 +1,4 @@
+//动态规划解法，O(N ^ 2)
 export function minSteps(n: number): number {
   //dp[i][j]表示要达到记事本上有i个字符，粘贴板上有j个字符这个状态，需要的最小的操作数
   //最后一次操作可能为Copy All或者Paste
@@ -36,4 +37,41 @@ export function minSteps(n: number): number {
   }
 
   return ans
+}
+
+//动态规划优化版递推公式,O(N * sqrt(N))
+export function minSteps1(n: number): number {
+  const dp: number[] = []
+  dp[1] = 0
+
+  //dp[i]表示要想达到记事本有i个字符需要的最小操作数
+  //我们可以把一次Copy All和若干次Paste联系在一起
+  //若Copy All之前记事本有j个字符(j是i的因子)，Copy All
+  //之后在经过i / j - 1粘贴操作就能达到dp[i]状态
+  //所以总共的操作就等于一次Copy All和 i / j - 1次Paste
+  //操作总共i / j 次,所以dp[i] = dp[j] + i / j
+  for (let i = 2; i <= n; ++i) {
+    dp[i] = Infinity
+    //如果j是i的因子，那么i / j也是i的因子，i * (i / j) = i
+    //要保证等式成立，不难看出i和i / j中其中必定有一个小于等于sqrt(i)
+    //所以只用在[1,sqrt(i)]范围内枚举j
+    for (let j = 1; j * j <= i; ++j) {
+      //只有j是i的因子时才进行状态转移
+      if (i % j === 0) {
+        dp[i] = Math.min(dp[i], 
+          //当前有j个字符，
+          //要达到i个字符要进行的操作:
+          // Copy All: 1
+          // Paste:    i / j - 1
+          dp[j] + i / j, 
+          //当前有i / j个字符
+          //要达到i个字符要进行的操作:
+          //Copy All: 1 
+          //Paste: j - 1
+          dp[i / j] + j)
+      }
+    }
+  }
+
+  return dp[n]
 }
