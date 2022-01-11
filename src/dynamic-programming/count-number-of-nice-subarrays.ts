a8b31035e273f9fcb79cc6575febda43d9d1616d
瞿贵友 <quguiyou1076@qq.com>
2022-01-11 10:44:47 +0800

    count-number-of-nice-subarrays

--- src/dynamic-programming/count-number-of-nice-subarrays.ts
+++ src/dynamic-programming/count-number-of-nice-subarrays.ts
@@ -1,13 +1,13 @@
-export function subarraySum(nums: number[], k: number): number {
+export function numberOfSubarrays(nums: number[], k: number): number {
   let presum = 0
   const presumToCount = new Map<number, number>()
   const n = nums.length
   presumToCount.set(0, 1)
+
   let ans = 0
   for (let i = 0; i < n; ++i) {
-    presum += nums[i]
+    presum = presum + (nums[i] % 2)
     ans += presumToCount.get(presum - k) ?? 0
-
     presumToCount.set(presum, (presumToCount.get(presum) ?? 0) + 1)
   }
 
