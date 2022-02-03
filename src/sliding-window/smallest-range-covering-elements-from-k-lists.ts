40f6edb0742b167b1b00e139a190b76b2fed04be
瞿贵友 <quguiyou1076@qq.com>
2022-02-03 22:00:49 +0800

    smallest-range-covering-elements-from-k-lists滑动窗口解法

--- src/sliding-window/smallest-range-covering-elements-from-k-lists.ts
+++ src/sliding-window/smallest-range-covering-elements-from-k-lists.ts
@@ -41,3 +41,61 @@ export function smallestRange(nums: number[][]): number[] {
 
   return [leftRange, rightRange]
 }
+
+export function smallestRange1(nums: number[][]): number[] {
+  const valToIndices = new Map<number, number[]>()
+  const n = nums.length
+  let min = Infinity
+  let max = -Infinity
+  for (let i = 0; i < n; ++i) {
+    for (let x of nums[i]) {
+      const list = valToIndices.get(x) ?? []
+      valToIndices.set(x, list)
+      list.push(i)
+      min = Math.min(min, x)
+      max = Math.max(max, x)
+    }
+  }
+
+  let l = min
+  let r = min - 1
+  let inside = 0
+  const fre: Record<number, number> = {}
+  let bestLeft = 0
+  let bestRight = Infinity
+
+  while (l <= max) {
+    while (inside < n) {
+      ++r
+      if (r > max) return [bestLeft, bestRight]
+      if (!valToIndices.has(r)) continue
+
+      for (const index of valToIndices.get(r)!) {
+        fre[index] = (fre[index] ?? 0) + 1
+
+        if (fre[index] === 1) {
+          ++inside
+        }
+      }
+    }
+
+    if (inside === n && r - l < bestRight - bestLeft) {
+      bestLeft = l
+      bestRight = r
+    }
+
+    if (valToIndices.has(l)) {
+      for (const index of valToIndices.get(l)!) {
+        --fre[index]
+
+        if (fre[index] === 0) {
+          --inside
+        }
+      }
+    }
+
+    ++l
+  }
+
+  return [bestLeft, bestRight]
+}
