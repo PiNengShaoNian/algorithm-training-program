c2509bee8aced5d845024f372b5105e940904459
laoqu <quguiyou1076@qq.com>
2022-09-10 16:48:07 +0800

    out-of-boundary-paths

--- src/dynamic-programming/out-of-boundary-paths.cpp
+++ src/dynamic-programming/out-of-boundary-paths.cpp
@@ -32,3 +32,38 @@ public:
         return ans;
     }
 };
+
+class Solution1 {
+public:
+    int mod = 1000000007;
+    int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int ans = 0;
+        // dp[i][j][k]表示由起点(startRow,startColumn)经过k步走动(i,j)总共有多少条路径
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove + 1, 0)));
+        dp[startRow][startColumn][0] = 1;
+
+        for(int step = 1; step <= maxMove; ++step) {
+            for(int i = 0; i < m; ++i) {
+                for(int j = 0; j < n; ++j) {
+                    if(dp[i][j][step - 1] == 0) continue;
+                    for(int k = 0; k < 4; ++k) {
+                        int new_row = i + dirs[k][0];
+                        int new_col = j + dirs[k][1];
+
+                        if(new_row < 0 || new_row >= m || new_col < 0 || new_col >= n) {
+                            ans += dp[i][j][step - 1];
+                            ans %= mod;
+                        }
+                        else {
+                            dp[new_row][new_col][step] += dp[i][j][step - 1];
+                            dp[new_row][new_col][step] %= mod; 
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
