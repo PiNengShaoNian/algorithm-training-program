cd4f60c53b99bbc2246e60e345a6c921d91d992c
瞿贵友 <quguiyou1076@qq.com>
2022-09-24 23:40:07 +0800

    word-ladder双向BFS解法

--- src/graph-and-tree/word-ladder.cpp
+++ src/graph-and-tree/word-ladder.cpp
@@ -52,3 +52,74 @@ public:
         return 0;
     }
 };
+
+//双向BFS解法
+class Solution {
+public:
+	unordered_map<string, string> edgeTo;
+
+	int expand(unordered_map<string, int>& distTo, unordered_map<string, int>& otherDistTo, queue<string>& q) {
+		queue<string> nextQ;
+		while (q.size()) {
+			string cur = q.front();
+			q.pop();
+			int wordLen = cur.size();
+			int depth = distTo[cur];
+			for (int i = 0; i < wordLen; ++i) {
+				string cand = cur;
+				for (int j = 0; j < 26; ++j) {
+					cand[i] = 'a' + j;
+
+					if (distTo.find(cand) == distTo.end()) continue;
+
+					if (otherDistTo[cand] != INT_MAX) {
+						return depth + otherDistTo[cand];
+					}
+
+					if (depth + 1 < distTo[cand]) {
+						nextQ.push(cand);
+						distTo[cand] = depth + 1;
+					}
+				}
+			}
+		}
+
+		while (nextQ.size()) {
+			q.push(nextQ.front());
+			nextQ.pop();
+		}
+		return -1;
+	}
+    
+	int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+		unordered_map<string, int> distToStart;
+		unordered_map<string, int> distToEnd;
+		queue<string> queueStart;
+		queue<string> queueEnd;
+
+		for (auto& word : wordList) {
+			distToStart[word] = INT_MAX;
+			distToEnd[word] = INT_MAX;
+		}
+
+		if (distToStart.find(endWord) == distToStart.end()) return 0;
+
+		queueStart.push(beginWord);
+		queueEnd.push(endWord);
+		distToStart[beginWord] = 1;
+		distToEnd[endWord] = 1;
+		distToEnd[beginWord] = INT_MAX;
+		distToStart[endWord] = INT_MAX;
+		while (queueStart.size() || queueEnd.size()) {
+			int res = expand(distToStart, distToEnd, queueStart);
+
+			if (res != -1) return res;
+
+			res = expand(distToEnd, distToStart, queueEnd);
+
+			if (res != -1) return res;
+		}
+
+		return 0;
+	}
+};
