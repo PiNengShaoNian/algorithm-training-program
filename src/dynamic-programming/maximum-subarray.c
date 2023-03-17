172f9f5a1f8ab6438eb9d5942612bf4cbfe9e774
quguiyou <quguiyou1076@qq.com>
2023-03-17 19:01:50 +0800

    maximum-subarray前缀和解法

--- src/dynamic-programming/maximum-subarray.c
+++ src/dynamic-programming/maximum-subarray.c
@@ -1,16 +1,41 @@
-int maxSubArray(int* nums, int numsSize){
-    int* dp = malloc(sizeof(int) * numsSize);
-    
+int maxSubArray(int *nums, int numsSize)
+{
+    int *dp = malloc(sizeof(int) * numsSize);
+
     int ans = nums[0];
     dp[0] = nums[0];
-    for(int i = 1; i < numsSize; ++i) {
+    for (int i = 1; i < numsSize; ++i)
+    {
         dp[i] = nums[i];
 
-        if(dp[i - 1] + nums[i] > nums[i]) {
+        if (dp[i - 1] + nums[i] > nums[i])
+        {
             dp[i] = dp[i - 1] + nums[i];
         }
 
         ans = ans > dp[i] ? ans : dp[i];
+    }
+
+    return ans;
+}
+
+int maxSubArray(int *nums, int numsSize)
+{
+    int *presum = malloc(sizeof(int) * (numsSize + 1));
+
+    presum[0] = 0;
+    for (int i = 1; i <= numsSize; ++i)
+    {
+        presum[i] = presum[i - 1] + nums[i - 1];
+    }
+
+    int presumMin = 0;
+    int ans = nums[0];
+    for (int i = 1; i <= numsSize; ++i)
+    {
+        ans = presum[i] - presumMin > ans ? presum[i] - presumMin : ans;
+
+        presumMin = presum[i] < presumMin ? presum[i] : presumMin;
     }
 
     return ans;
