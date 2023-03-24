836f4185c2450ea30c7742748b91e82b822ae780
quguiyou <quguiyou1076@qq.com>
2023-03-24 21:36:10 +0800

    maximum-sum-circular-subarray单调队列前缀和解法

--- src/dynamic-programming/maximum-sum-circular-subarray.cpp
+++ src/dynamic-programming/maximum-sum-circular-subarray.cpp
@@ -19,3 +19,38 @@ public:
         return max(maxSum, sum - minSum == 0 ? maxSum : sum - minSum);
     }
 };
+
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& _nums) {
+        vector<int> nums(_nums.begin(), _nums.end());
+        int n = _nums.size();
+
+        for(int i = 0; i < n; ++i) {
+            nums.push_back(_nums[i]);
+        }
+
+        vector<int> presum(2 * n + 1, 0);
+
+        presum[0] = 0;
+
+        for(int i = 1; i <= 2 * n; ++i) {
+            presum[i] = presum[i - 1] + nums[i - 1];
+        }
+
+        deque<int> dq;
+        int ans = nums[0];
+        for(int i = 0; i <= 2 * n; ++i) {
+            while(dq.size() && i - dq.front() >= n + 1) dq.pop_front();
+
+            if(dq.size())
+                ans = max(ans, presum[i] - presum[dq.front()]);
+
+            while(dq.size() && presum[dq.back()] >= presum[i]) dq.pop_back();
+
+            dq.push_back(i);
+        }
+
+        return ans;
+    }
+};
