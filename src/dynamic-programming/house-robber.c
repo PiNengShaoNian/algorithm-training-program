abf9ed6985e3a9c770a7b3a09f5828a2076545c0
quguiyou <quguiyou1076@qq.com>
2023-03-29 09:00:27 +0800

    house-robber

---
+++ src/dynamic-programming/house-robber.c
@@ -0,0 +1,19 @@
+int rob(int *nums, int numsSize)
+{
+    if (numsSize == 1)
+        return nums[0];
+
+    // dp[i]表示在只在区间dp[0:i]内偷窃的情况下能获得的最大收益
+    int dp[numsSize];
+
+#define MAX(a, b) (a) > (b) ? (a) : (b)
+
+    dp[0] = nums[0];
+    dp[1] = MAX(dp[0], nums[1]);
+    for (int i = 2; i < numsSize; ++i)
+    {
+        dp[i] = MAX(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+
+    return dp[numsSize - 1];
+}
