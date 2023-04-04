d71c97a4e4a63f95bb6c3c521ab25a0f53712931
quguiyou <quguiyou1076@qq.com>
2023-04-04 14:24:44 +0800

    decode-ways

---
+++ src/dynamic-programming/decode-ways.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int numDecodings(string s)
+    {
+        if (s[0] == '0')
+            return 0;
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        dp[n] = 1;
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (s[i] == '0')
+            {
+                dp[i] = 0;
+            }
+            else
+            {
+                dp[i] = dp[i + 1];
+                if (i + 1 < n && ((s[i] - '0') * 10 + s[i + 1] - '0') <= 26)
+                {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
