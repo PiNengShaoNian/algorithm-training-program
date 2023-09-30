48033beb5bcbf366be769db8f4d39506b1c9af15
quguiyou <quguiyou1076@qq.com>
2023-09-30 10:53:36 +0800

    candy

---
+++ src/other/candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+ public:
+  int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    vector<int> left(n);
+
+    for (int i = 0; i < n; i++) {
+      if (i > 0 && ratings[i] > ratings[i - 1]) {
+        left[i] = left[i - 1] + 1;
+      } else {
+        left[i] = 1;
+      }
+    }
+
+    int ans = 0;
+    int right = 0;
+
+    for (int i = n - 1; i >= 0; i--) {
+      if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+        right++;
+      } else {
+        right = 1;
+      }
+
+      ans += max(left[i], right);
+    }
+
+    return ans;
+  }
+};
