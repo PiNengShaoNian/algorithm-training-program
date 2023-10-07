ac803da7f6acda7bf41d3c8e778eae8979791a0d
quguiyou <quguiyou1076@qq.com>
2023-10-07 22:29:55 +0800

    fraction-to-recurring-decimal

---
+++ src/other/fraction-to-recurring-decimal.cpp
@@ -0,0 +1,42 @@
+class Solution {
+ public:
+  string fractionToDecimal(int numerator, int denominator) {
+    long numeratorLong = numerator;
+    long denominatorLong = denominator;
+    if (numeratorLong % denominatorLong == 0) {
+      return to_string(numeratorLong / denominatorLong);
+    }
+
+    string ans;
+    if (numeratorLong < 0 ^ denominatorLong < 0) {
+      ans.push_back('-');
+    }
+
+    numeratorLong = abs(numeratorLong);
+    denominatorLong = abs(denominatorLong);
+    long integerPart = numeratorLong / denominatorLong;
+    ans += to_string(integerPart);
+    ans.push_back('.');
+
+    string fractionPart;
+    unordered_map<long, int> remainderIndexMap;
+    long remainder = numeratorLong % denominatorLong;
+    int index = 0;
+    while (remainder != 0 && !remainderIndexMap.count(remainder)) {
+      remainderIndexMap[remainder] = index;
+      remainder *= 10;
+      fractionPart += to_string(remainder / denominatorLong);
+      remainder %= denominatorLong;
+      index++;
+    }
+    if (remainder != 0) {
+      int insertIndex = remainderIndexMap[remainder];
+      fractionPart = fractionPart.substr(0, insertIndex) + '(' +
+                     fractionPart.substr(insertIndex);
+      fractionPart.push_back(')');
+    }
+    ans += fractionPart;
+
+    return ans;
+  }
+};
