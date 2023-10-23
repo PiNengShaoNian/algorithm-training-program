06c4043a65a3fc2151bb0e065925184c553b2af8
quguiyou <quguiyou1076@qq.com>
2023-10-23 21:37:38 +0800

    random-pick-index

---
+++ src/other/random-pick-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  unordered_map<int, vector<int>> pos;
+
+ public:
+  Solution(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+      pos[nums[i]].push_back(i);
+    }
+  }
+
+  int pick(int target) {
+    auto& indices = pos[target];
+    return indices[rand() % indices.size()];
+  }
+};
