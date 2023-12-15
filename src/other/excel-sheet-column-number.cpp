bcd4dc93bb07b74c1be8843f8164e5e7b1414fc9
quguiyou <quguiyou1076@qq.com>
2023-12-15 23:43:30 +0800

    excel-sheet-column-number

---
+++ src/other/excel-sheet-column-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  int titleToNumber(string columnTitle) {
+    int number = 0;
+    long multiple = 1;
+    for (int i = columnTitle.size() - 1; i >= 0; i--) {
+      int k = columnTitle[i] - 'A' + 1;
+      number += k * multiple;
+      multiple *= 26;
+    }
+    return number;
+  }
+};
