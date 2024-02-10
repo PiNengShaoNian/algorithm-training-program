d9a4ceba5fd423468148c79c905412ab3b356b96
quguiyou <quguiyou1076@qq.com>
2024-02-11 00:46:08 +0800

    rotated-digits

---
+++ src/other/rotated-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+ public:
+  int rotatedDigits(int n) {
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+      bool valid = true, diff = false;
+      string num = to_string(i);
+      for (char ch : num) {
+        if (check[ch - '0'] == -1) {
+          valid = false;
+        } else if (check[ch - '0'] == 1) {
+          diff = true;
+        }
+      }
+      if (valid && diff) {
+        ans++;
+      }
+    }
+    return ans;
+  }
+
+ private:
+  static constexpr int check[10] = {0, 0, 1, -1, -1, 1, 1, -1, 0, 1};
+};
