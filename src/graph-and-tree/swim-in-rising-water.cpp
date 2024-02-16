55428c5a1c85be02b13d71157410868241f5446e
quguiyou <quguiyou1076@qq.com>
2024-02-16 22:38:39 +0800

    swim-in-rising-water并查集解法

--- src/graph-and-tree/swim-in-rising-water.cpp
+++ src/graph-and-tree/swim-in-rising-water.cpp
@@ -45,3 +45,70 @@ class Solution {
 
   bool inArea(int x, int y) { return x >= 0 && x < N && y >= 0 && y < N; }
 };
+
+class UnionFind {
+ public:
+  UnionFind(int n) {
+    data = vector<int>(n);
+    for (int i = 0; i < n; i++) {
+      data[i] = i;
+    }
+  }
+
+  int find(int x) { return x == data[x] ? x : (data[x] = find(data[x])); }
+
+  int isConnected(int a, int b) { return find(a) == find(b); }
+
+  void connect(int a, int b) {
+    int p1 = find(a);
+    int p2 = find(b);
+    if (p1 == p2) {
+      return;
+    }
+    data[p1] = p2;
+  }
+
+ private:
+  vector<int> data;
+};
+
+class Solution1 {
+ public:
+  int swimInWater(vector<vector<int>> &grid) {
+    N = grid.size();
+    int len = N * N;
+    vector<int> index(len, 0);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        index[grid[i][j]] = getIndex(i, j);
+      }
+    }
+
+    UnionFind unionFind(len);
+    for (int i = 0; i < len; i++) {
+      int x = index[i] / N;
+      int y = index[i] % N;
+
+      for (auto &&direction : DIRECTIONS) {
+        int newX = x + direction[0];
+        int newY = y + direction[1];
+        if (inArea(newX, newY) && grid[newX][newY] <= i) {
+          unionFind.connect(index[i], getIndex(newX, newY));
+        }
+
+        if (unionFind.isConnected(0, len - 1)) {
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
+
+ private:
+  vector<vector<int>> DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+  int N;
+
+  int getIndex(int x, int y) { return x * N + y; }
+
+  bool inArea(int x, int y) { return x >= 0 && x < N && y >= 0 && y < N; }
+};
