eb9883fcb5868650e2a67ed12f1f2198b2f7cc2d
quguiyou <quguiyou1076@qq.com>
2024-02-19 22:32:39 +0800

    number-of-matching-subsequences多指针解法

--- src/binary-search/number-of-matching-subsequences.cpp
+++ src/binary-search/number-of-matching-subsequences.cpp
@@ -29,3 +29,31 @@ class Solution {
     return ans;
   }
 };
+
+class Solution {
+ public:
+  int numMatchingSubseq(string s, vector<string> &words) {
+    vector<queue<pair<int, int>>> queues(26);
+    for (int i = 0; i < words.size(); i++) {
+      queues[words[i][0] - 'a'].push({i, 0});
+    }
+
+    int ans = 0;
+    for (char c : s) {
+      auto &q = queues[c - 'a'];
+      int size = q.size();
+      while (size--) {
+        auto [i, j] = q.front();
+        q.pop();
+
+        j++;
+        if (j == words[i].size()) {
+          ans++;
+        } else {
+          queues[words[i][j] - 'a'].push({i, j});
+        }
+      }
+    }
+    return ans;
+  }
+};
