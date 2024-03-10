7e64c59ddcef44c27601402da44b5e1ec233d44d
quguiyou <quguiyou1076@qq.com>
2024-03-10 23:00:42 +0800

    friends-of-appropriate-ages前缀和解法

--- src/other/friends-of-appropriate-ages.cpp
+++ src/other/friends-of-appropriate-ages.cpp
@@ -25,3 +25,25 @@ class Solution {
     return ans;
   }
 };
+
+class Solution {
+ public:
+  int numFriendRequests(vector<int>& ages) {
+    vector<int> cnt(121);
+    for (int age : ages) {
+      cnt[age]++;
+    }
+    vector<int> pre(121);
+    for (int i = 1; i < 121; i++) {
+      pre[i] = pre[i - 1] + cnt[i];
+    }
+    int ans = 0;
+    for (int i = 15; i <= 120; i++) {
+      if (cnt[i]) {
+        int bound = i * 0.5 + 8;
+        ans += cnt[i] * (pre[i] - pre[bound - 1] - 1);
+      }
+    }
+    return ans;
+  }
+};
