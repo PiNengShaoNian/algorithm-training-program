f9168854b3c43fb9106ccd9c9f6a85a1ff28e5c7
quguiyou <quguiyou1076@qq.com>
2024-08-30 23:03:02 +0800

    sum-of-digit-differences-of-all-pairs

---
+++ src/string-and-array/sum-of-digit-differences-of-all-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  long long sumDigitDifferences(vector<int>& nums) {
+    long long res = 0;
+    int n = nums.size();
+    while (nums[0] > 0) {
+      vector<int> cnt(10);
+      for (int i = 0; i < n; i++) {
+        cnt[nums[i] % 10]++;
+        nums[i] /= 10;
+      }
+      for (int i = 0; i < 10; i++) {
+        res += (long long)(n - cnt[i]) * cnt[i];
+      }
+    }
+    return res / 2;
+  }
+};
