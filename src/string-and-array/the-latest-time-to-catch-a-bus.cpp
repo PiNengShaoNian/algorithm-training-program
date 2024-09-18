9fa7fd310403f870b6e0f54c08d0de7263c55f4d
quguiyou <quguiyou1076@qq.com>
2024-09-18 22:13:20 +0800

    the-latest-time-to-catch-a-bus

---
+++ src/string-and-array/the-latest-time-to-catch-a-bus.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int latestTimeCatchTheBus(vector<int>& buses, vector<int>& passengers, int capacity) {
+        sort(buses.begin(), buses.end());
+        sort(passengers.begin(), passengers.end());
+        int pos = 0;
+        int space = 0;
+        for (int arrive : buses) {
+            space = capacity;
+            while (space > 0 && pos < passengers.size() && passengers[pos] <= arrive) {
+                space--;
+                pos++;
+            }
+        }
+
+        pos--;
+        int lastCacheTime = space > 0 ? buses.back() : passengers[pos];
+        while (pos >= 0 && passengers[pos] == lastCacheTime) {
+            lastCacheTime--;
+            pos--;
+        }
+        return lastCacheTime;
+    }
+};
