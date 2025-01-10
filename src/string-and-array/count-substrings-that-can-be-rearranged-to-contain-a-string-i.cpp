1bd7bb2c98eb78a8d0e1475d13bd08275ecc8c05
quguiyou <quguiyou1076@qq.com>
2025-01-10 22:08:46 +0800

    count-substrings-that-can-be-rearranged-to-contain-a-string-i

--- src/string-and-array/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ src/string-and-array/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -31,3 +31,51 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    long long validSubstringCount(string word1, string word2) {
+        vector<int> count(26);
+        for (char c : word2) {
+            count[c - 'a']++;
+        }
+
+        int n = word1.size();
+        vector<vector<int>> pre_count(n + 1, vector<int>(26, 0));
+        for (int i = 1; i <= n; i++) {
+            pre_count[i].assign(pre_count[i - 1].begin(), pre_count[i - 1].end());
+            pre_count[i][word1[i - 1] - 'a']++;
+        }
+
+        auto get = [&](int l) -> int {
+            int border = l;
+            int r = n;
+            int ans = n + 1;
+            while (l <= r) {
+                int mid = (r - l) / 2 + l;
+                int f = true;
+                for (int i = 0; i < 26; i++) {
+                    if (pre_count[mid][i] - pre_count[border - 1][i] < count[i]) {
+                        f = false;
+                        break;
+                    }
+                }
+
+                if (f) {
+                    ans = mid;
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return ans;
+        };
+
+        long long res = 0;
+        for (int i = 1; i <= n; i++) {
+            int r = get(i);
+            res += n - r + 1;
+        }
+        return res;
+    }
+};
