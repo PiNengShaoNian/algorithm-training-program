4ad674b0228f7bec9ed11122d5be0db508bb974a
quguiyou <quguiyou1076@qq.com>
2025-01-17 22:45:25 +0800

    shortest-subarray-with-or-at-least-k-i

--- src/sliding-window/shortest-subarray-with-or-at-least-k-i.cpp
+++ src/sliding-window/shortest-subarray-with-or-at-least-k-i.cpp
@@ -31,3 +31,22 @@ public:
         return ans == INT_MAX ? -1 : ans;
     }
 };
+
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int res = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int value = 0;
+            for (int j = i; j < n; j++) {
+                value |= nums[j];
+                if (value >= k) {
+                    res = min(res, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
