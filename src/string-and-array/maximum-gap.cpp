960fc6242a2e39efe54ebf0234a1f0cdfbae089a
quguiyou <quguiyou1076@qq.com>
2025-04-12 23:27:10 +0800

    maximum-gap

---
+++ src/string-and-array/maximum-gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int maximumGap(vector<int>& nums) {
+            int n = nums.size();
+            if (n <= 1) {
+                return 0;
+            }
+            sort(nums.begin(), nums.end());
+    
+            int ans = nums[1] - nums[0];
+            for (int i = 1; i < n; i++) {
+                ans = max(ans, nums[i] - nums[i - 1]);
+            }
+            return ans;
+        }
+    };
