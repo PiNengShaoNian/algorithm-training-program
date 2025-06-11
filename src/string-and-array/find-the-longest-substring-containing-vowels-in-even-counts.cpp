41aa007e831796ece04cfc9782c2ae8c79cb8c67
quguiyou <quguiyou1076@qq.com>
2025-06-11 23:13:36 +0800

    find-the-longest-substring-containing-vowels-in-even-counts

---
+++ src/string-and-array/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,29 @@
+class Solution {
+ public:
+  int findTheLongestSubstring(string s) {
+    int ans = 0;
+    int status = 0;
+    int n = s.size();
+    vector<int> pos(1 << 5, -1);
+    pos[0] = 0;
+    for (int i = 0; i < n; i++) {
+      if (s[i] == 'a') {
+        status ^= 1 << 0;
+      } else if (s[i] == 'e') {
+        status ^= 1 << 1;
+      } else if (s[i] == 'i') {
+        status ^= 1 << 2;
+      } else if (s[i] == 'o') {
+        status ^= 1 << 3;
+      } else if (s[i] == 'u') {
+        status ^= 1 << 4;
+      }
+      if (~pos[status]) {
+        ans = max(ans, i + 1 - pos[status]);
+      } else {
+        pos[status] = i + 1;
+      }
+    }
+    return ans;
+  }
+};
