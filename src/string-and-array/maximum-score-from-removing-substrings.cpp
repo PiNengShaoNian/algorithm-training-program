b48a732b2de755bfa827cf4ba1748c2e7bc89f33
quguiyou <quguiyou1076@qq.com>
2025-07-23 22:10:48 +0800

    maximum-score-from-removing-substrings

---
+++ src/string-and-array/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+ public:
+  int maximumGain(string s, int x, int y) {
+    char a = 'a';
+    char b = 'b';
+    if (x < y) {
+      swap(x, y);
+      swap(a, b);
+    }
+
+    int cnt1 = 0;
+    int cnt2 = 0;
+    int ans = 0;
+    for (char c : s) {
+      if (c == a) {
+        cnt1++;
+      } else if (c == b) {
+        if (cnt1) {
+          ans += x;
+          cnt1--;
+        } else {
+          cnt2++;
+        }
+      } else {
+        ans += min(cnt1, cnt2) * y;
+        cnt1 = 0;
+        cnt2 = 0;
+      }
+    }
+
+    ans += min(cnt1, cnt2) * y;
+    return ans;
+  }
+};
