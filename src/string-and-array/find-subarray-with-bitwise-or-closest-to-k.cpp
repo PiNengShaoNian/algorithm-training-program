dd1841968316efe9e98fef4fa20eebc4cbe5daeb
quguiyou <quguiyou1076@qq.com>
2025-07-29 21:44:29 +0800

    find-subarray-with-bitwise-or-closest-to-k

--- src/string-and-array/find-subarray-with-bitwise-or-closest-to-k.cpp
+++ src/string-and-array/find-subarray-with-bitwise-or-closest-to-k.cpp
@@ -1,34 +1,51 @@
 class Solution {
-public:
-    int minimumDifference(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> bits_max_pos(31, -1);
-        vector<pair<int, int>> pos_to_bit;
-        int res = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= 30; j++) {
-                if (nums[i] >> j & 1) {
-                    bits_max_pos[j] = i;
-                }
-            }
-
-            pos_to_bit.clear();
-            for (int j = 0; j <= 30; j++) {
-                if (bits_max_pos[j] != -1) {
-                    pos_to_bit.push_back(make_pair(bits_max_pos[j], j));
-                }
-            }
-            sort(pos_to_bit.begin(), pos_to_bit.end(), greater<pair<int, int>>());
-            int val = 0;
-            for (int j = 0, p = 0; j < pos_to_bit.size();) {
-                while (j < pos_to_bit.size() && pos_to_bit[j].first == pos_to_bit[p].first) {
-                    val |= 1 << pos_to_bit[j].second;
-                    j++;
-                }
-                p = j;
-                res = min(res, abs(k - val));
-            }
+ public:
+  int minimumDifference(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> bits_max_pos(31, -1);
+    vector<pair<int, int>> pos_to_bit;
+    int res = INT_MAX;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= 30; j++) {
+        if (nums[i] >> j & 1) {
+          bits_max_pos[j] = i;
         }
-        return res;
+      }
+
+      pos_to_bit.clear();
+      for (int j = 0; j <= 30; j++) {
+        if (bits_max_pos[j] != -1) {
+          pos_to_bit.push_back(make_pair(bits_max_pos[j], j));
+        }
+      }
+      sort(pos_to_bit.begin(), pos_to_bit.end(), greater<pair<int, int>>());
+      int val = 0;
+      for (int j = 0, p = 0; j < pos_to_bit.size();) {
+        while (j < pos_to_bit.size() &&
+               pos_to_bit[j].first == pos_to_bit[p].first) {
+          val |= 1 << pos_to_bit[j].second;
+          j++;
+        }
+        p = j;
+        res = min(res, abs(k - val));
+      }
     }
+    return res;
+  }
+};
+
+class Solution {
+ public:
+  int minimumDifference(vector<int>& nums, int k) {
+    int ans = INT_MAX;
+    for (int i = 0; i < nums.size(); i++) {
+      int x = nums[i];
+      ans = min(ans, abs(k - x));
+      for (int j = i - 1; j >= 0 && (nums[j] | x) != nums[j]; j--) {
+        nums[j] |= x;
+        ans = min(ans, abs(k - nums[j]));
+      }
+    }
+    return ans;
+  }
 };
