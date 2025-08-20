144f715f6ad33e30ba41aebc6578d6accb272a7a
quguiyou <quguiyou1076@qq.com>
2025-08-20 21:45:05 +0800

    count-square-submatrices-with-all-ones

--- src/dynamic-programming/count-square-submatrices-with-all-ones.cpp
+++ src/dynamic-programming/count-square-submatrices-with-all-ones.cpp
@@ -1,28 +1,44 @@
 class Solution {
-public:
-    int countSquares(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        //dp[i][j]表示以[i,j]为右下角的正方形数量
-        vector<vector<int>> dp(n,vector<int>(m, 0));
-        
-        int ans = 0;
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                if(i == 0 && j == 0) {
-                    dp[i][j] = matrix[0][0];
-                }
-                else if(i == 0 || j == 0) {
-                    dp[i][j] = matrix[i][j];
-                }
-                else if(matrix[i][j] == 1){
-                    dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-                }
-            
-                ans += dp[i][j];
-            }
+ public:
+  int countSquares(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    // dp[i][j]表示以[i,j]为右下角的正方形数量
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < m; ++j) {
+        if (i == 0 && j == 0) {
+          dp[i][j] = matrix[0][0];
+        } else if (i == 0 || j == 0) {
+          dp[i][j] = matrix[i][j];
+        } else if (matrix[i][j] == 1) {
+          dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
         }
 
-        return ans;
+        ans += dp[i][j];
+      }
     }
+
+    return ans;
+  }
+};
+
+class Solution {
+ public:
+  int countSquares(vector<vector<int>>& matrix) {
+    int m = matrix.size(), n = matrix[0].size();
+    vector f(m + 1, vector<int>(n + 1));
+    int ans = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j]) {
+          f[i + 1][j + 1] = min({f[i][j], f[i][j + 1], f[i + 1][j]}) + 1;
+          ans += f[i + 1][j + 1];
+        }
+      }
+    }
+    return ans;
+  }
 };
