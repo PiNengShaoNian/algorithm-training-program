1b294249ec050100dc320e35a1eba24d3eff83e9
quguiyou <quguiyou1076@qq.com>
2025-09-18 22:40:47 +0800

    design-task-manager

---
+++ src/other/design-task-manager.cpp
@@ -0,0 +1,52 @@
+class TaskManager {
+ private:
+  unordered_map<int, pair<int, int>> taskInfo;
+  priority_queue<pair<int, int>> heap;
+
+ public:
+  TaskManager(vector<vector<int>> tasks) {
+    for (auto& task : tasks) {
+      int userId = task[0], taskId = task[1], priority = task[2];
+      taskInfo[taskId] = {priority, userId};
+      heap.emplace(priority, taskId);
+    }
+  }
+
+  void add(int userId, int taskId, int priority) {
+    taskInfo[taskId] = {priority, userId};
+    heap.emplace(priority, taskId);
+  }
+
+  void edit(int taskId, int newPriority) {
+    if (taskInfo.find(taskId) != taskInfo.end()) {
+      taskInfo[taskId].first = newPriority;
+      heap.emplace(newPriority, taskId);
+    }
+  }
+
+  void rmv(int taskId) { taskInfo.erase(taskId); }
+
+  int execTop() {
+    while (!heap.empty()) {
+      auto [priority, taskId] = heap.top();
+      heap.pop();
+
+      if (taskInfo.find(taskId) != taskInfo.end() &&
+          taskInfo[taskId].first == priority) {
+        int userId = taskInfo[taskId].second;
+        taskInfo.erase(taskId);
+        return userId;
+      }
+    }
+    return -1;
+  }
+};
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
