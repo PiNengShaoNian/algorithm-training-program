4dca0f6832d3dc0e092619a71fc03427bf85a082
quguiyou <quguiyou1076@qq.com>
2025-09-28 21:50:33 +0800

    largest-perimeter-triangle

---
+++ src/string-and-array/largest-perimeter-triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+  int largestPerimeter(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    for (int i = (int)nums.size() - 1; i >= 2; i--) {
+      if (nums[i - 2] + nums[i - 1] > nums[i]) {
+        return nums[i - 2] + nums[i - 1] + nums[i];
+      }
+    }
+    return 0;
+  }
+};
