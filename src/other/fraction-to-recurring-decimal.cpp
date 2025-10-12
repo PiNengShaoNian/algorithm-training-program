8baa21e65f30d2481672175e5eb236fb2a1bfa9f
quguiyou <quguiyou1076@qq.com>
2025-10-12 19:24:43 +0800

    fraction-to-recurring-decimal

--- src/other/fraction-to-recurring-decimal.cpp
+++ src/other/fraction-to-recurring-decimal.cpp
@@ -40,3 +40,35 @@ class Solution {
     return ans;
   }
 };
+
+class Solution {
+ public:
+  string fractionToDecimal(int numerator, int denominator) {
+    long long a = numerator;
+    long long b = denominator;
+    string sign = a * b < 0 ? "-" : "";
+    a = abs(a);
+    b = abs(b);
+
+    long long q = a / b;
+    long long r = a % b;
+    if (r == 0) {
+      return sign + to_string(q);
+    }
+
+    string ans = sign + to_string(q) + ".";
+    unordered_map<long long, int> r_to_pos = {{r, ans.size()}};
+    while (r) {
+      r *= 10;
+      q = r / b;
+      r %= b;
+      ans += '0' + q;
+      if (r_to_pos.contains(r)) {
+        int pos = r_to_pos[r];
+        return ans.substr(0, pos) + "(" + ans.substr(pos) + ")";
+      }
+      r_to_pos[r] = ans.size();
+    }
+    return ans;
+  }
+};
