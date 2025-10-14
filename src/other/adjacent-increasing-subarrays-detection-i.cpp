927b574212ed9a417d19ac62f13706d0e0e8e063
quguiyou <quguiyou1076@qq.com>
2025-10-14 22:29:24 +0800

    adjacent-increasing-subarrays-detection-i

---
+++ src/other/adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+    int n = nums.size();
+    int cnt = 1;
+    int precnt = 0;
+    int ans = 0;
+    for (int i = 1; i < n; i++) {
+      if (nums[i] > nums[i - 1]) {
+        cnt++;
+      } else {
+        precnt = cnt;
+        cnt = 1;
+      }
+
+      ans = max(ans, min(precnt, cnt));
+      ans = max(ans, cnt / 2);
+    }
+    return ans >= k;
+  }
+};
