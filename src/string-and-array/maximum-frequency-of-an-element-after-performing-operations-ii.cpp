7cf42ef4cb0b8aabf50374b394d1d0b58e423e9f
quguiyou <quguiyou1076@qq.com>
2025-10-21 22:29:24 +0800

    maximum-frequency-of-an-element-after-performing-operations-ii

---
+++ src/string-and-array/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  int maxFrequency(vector<int>& nums, int k, int numOperations) {
+    unordered_map<int, int> cnt;
+    map<int, int> diff;
+    for (int x : nums) {
+      cnt[x]++;
+      diff[x];        // 把 x 插入 diff，以保证下面能遍历到 x
+      diff[x - k]++;  // 把 [x-k, x+k] 中的每个整数的出现次数都加一
+      diff[x + k + 1]--;
+    }
+
+    int ans = 0, sum_d = 0;
+    for (auto& [x, d] : diff) {
+      sum_d += d;
+      ans = max(ans, min(sum_d, cnt[x] + numOperations));
+    }
+    return ans;
+  }
+};
