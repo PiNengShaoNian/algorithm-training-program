0acbb26eef96f4404befa05b759bde87b848c4f9
quguiyou <quguiyou1076@qq.com>
2025-11-20 23:06:12 +0800

    keep-multiplying-found-values-by-two

--- src/string-and-array/keep-multiplying-found-values-by-two.cpp
+++ src/string-and-array/keep-multiplying-found-values-by-two.cpp
@@ -10,3 +10,16 @@ class Solution {
     return original;
   }
 };
+
+class Solution {
+ public:
+  int findFinalValue(vector<int>& nums, int original) {
+    sort(nums.begin(), nums.end());
+    for (int num : nums) {
+      if (original == num) {
+        original *= 2;
+      }
+    }
+    return original;
+  }
+};
