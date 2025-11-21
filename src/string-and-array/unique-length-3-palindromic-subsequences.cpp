fb13e708bb3a0ccc83c919f6e07b50999770cb1f
quguiyou <quguiyou1076@qq.com>
2025-11-21 21:36:29 +0800

    unique-length-3-palindromic-subsequences

---
+++ src/string-and-array/unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+ public:
+  int countPalindromicSubsequence(string s) {
+    int n = s.size();
+    int res = 0;
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+      int l = 0;
+      int r = n - 1;
+      while (l < n && s[l] != ch) {
+        l++;
+      }
+
+      while (r >= 0 && s[r] != ch) {
+        r--;
+      }
+
+      if (r - l < 2) {
+        continue;
+      }
+
+      unordered_set<char> charset;
+      for (int k = l + 1; k < r; k++) {
+        charset.insert(s[k]);
+      }
+      res += charset.size();
+    }
+    return res;
+  }
+};
