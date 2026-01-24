afe406feff7532859879d813c010d0c3d66a1970
quguiyou <quguiyou1076@qq.com>
2026-01-24 19:04:46 +0800

    minimize-maximum-pair-sum-in-array

---
+++ src/string-and-array/minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int minPairSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int l = 0;
+    int r = nums.size() - 1;
+    int ans = INT_MIN;
+    while (l < r) {
+      ans = max(ans, nums[l] + nums[r]);
+      l++;
+      r--;
+    }
+    return ans;
+  }
+};
